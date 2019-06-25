17f7219798267fc3194b97937f636db6a09447a0
Egil <egil.moller@freecode.no>
2019-06-25 18:06:34 +0200

    Moved the tcp handling to a separate library

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN apt update
 RUN apt install -y python3 python3-pip
 
 RUN pip3 install libais==0.17
+RUN pip3 install socket-tentacles
 
 ADD server.sh /server.sh
 ADD geocloud_nmea.py /geocloud_nmea.py
--- geocloud_nmea.py
+++ geocloud_nmea.py
@@ -1,5 +1,4 @@
-import socketserver
-import socket
+import socket_tentacles
 import ais.stream
 import ais.compatibility.gpsd
 import sys
@@ -11,39 +10,9 @@ import datetime
 
 senders = set()
 
-class TCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    def server_bind(self):
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind(self.server_address)
-        
-class Server(threading.Thread):
-    def run(self):
-        print("Server: Started: %s" % self._kwargs)
-        Handler = self._kwargs["handler"]
-        class Server(socketserver.BaseRequestHandler):
-            def handle(self):
-                print("Server: Connection request received")
-                Handler(self.request)
-        self.server = TCPServer((self._kwargs["host"], self._kwargs["port"]), Server)
-        self.server.serve_forever()
-
-class Connector(threading.Thread):
-    def run(self):
-        print("Connector: Started: %s" % self._kwargs)
-        while True:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                sock.connect((self._kwargs["host"], self._kwargs["port"]))
-                print("Connector: Connected")
-                self._kwargs["handler"](sock)
-            finally:
-                sock.close()
-            time.sleep(1)
-
-class ReceiveHandler(object):
-    def __init__(self, conn):
-        self.conn = conn.makefile("r")
-        for msg in ais.stream.decode(self.conn, keep_nmea=True):
+class ReceiveHandler(socket_tentacles.ReceiveHandler):
+    def handle(self):
+        for msg in ais.stream.decode(self.file, keep_nmea=True):
             msg = ais.compatibility.gpsd.mangle(msg)
             if "tagblock_timestamp" not in msg:
                 msg["tagblock_timestamp"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
@@ -54,18 +23,17 @@ class ReceiveHandler(object):
                 except Exception as e:
                     print(e)
     
-class SendHandler(object):
-    def __init__(self, conn):
-        self.conn = conn.makefile("w")
+class SendHandler(socket_tentacles.SendHandler):
+    def handle(self):
         self.queue = queue.Queue()
         try:
             senders.add(self)
             
             while True:
                 msg = self.queue.get()
-                json.dump(msg, self.conn)
-                self.conn.write("\n")
-                self.conn.flush()
+                json.dump(msg, self.file)
+                self.file.write("\n")
+                self.file.flush()
         finally:
             senders.remove(self)
 
@@ -79,16 +47,4 @@ if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         config = json.load(f)
 
-    for connection in config["connections"]:
-        handler = {"source": ReceiveHandler, "destination": SendHandler}[connection["direction"]]
-        addr = connection["address"].split(":")
-        assert addr[0] == "tcp"
-        host = "0.0.0.0"
-        port = 1024
-        if len(addr) == 2:
-            port = addr[1]
-        if len(addr) == 3:
-            host, port = addr[1:]
-        port = int(port)
-        connhandler = {"listen": Server, "connect": Connector}[connection["type"]]
-        connhandler(kwargs={"host": host, "port": port, "handler": handler}).start()
+    socket_tentacles.run(config, {"source": ReceiveHandler, "destination": SendHandler})
